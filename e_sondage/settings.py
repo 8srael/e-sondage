e8247a75510ff97c1e5d1ca392bca006b3d577b7
8srael <kouassiisrael36@gmail.com>
2024-05-08 00:34:41 +0000

    mysqlclient installed for mysql DBMS✅

--- e_sondage/settings.py
+++ e_sondage/settings.py
@@ -77,6 +77,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'e_sondage_db',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
     }
 }
 
