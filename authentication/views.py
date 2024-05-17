89efd1eb24aada75a4f9715c1c2189408c013b56
8srael <kouassiisrael36@gmail.com>
2024-05-17 14:33:02 +0000

    add some conditions for login authentication🆗

--- authentication/views.py
+++ authentication/views.py
@@ -18,6 +18,10 @@ def user_login(request):
         password = request.POST['password']
         # print(email, password, request is None)
         # user = authenticate(request, username=username, password=password)
+        
+        if(len(password) < 8):
+            messages.error(request, 'Le mot de passe doit contenir au moins 8 caractères')
+            return  render(request, 'login_and_register.html', {'showLogin':True, 'email': email})
         user = authenticate(request, email=email, password=password)
         if user:
             login(request, user)    # we can use alogin which is a asynvhrone method for login a user
@@ -25,7 +29,7 @@ def user_login(request):
         else:
             print('Out')
             messages.error(request, 'Email ou mot de passe incorrect')
-            return render(request, 'login_and_register.html', {'showLogin':True})
+            return render(request, 'login_and_register.html', {'showLogin':True, 'email': email})
     else: 
         return redirect('login_and_register')
 
@@ -38,12 +42,13 @@ def user_register(request):
         
         print(username, email, password, confirm_password)
 
+        data = { 'username': username, 'email': email }
         if(len(password) < 8):
             messages.error(request, 'Le mot de passe doit contenir au moins 8 caractères')
-            return  render(request, 'login_and_register.html', {'stay': True, 'showRegister':True})
+            return  render(request, 'login_and_register.html', {'stay': True, 'showRegister':True, 'data': data})
         if(password != confirm_password):
             messages.error(request, 'Les mots de passe ne correspondent pas')
-            return  render(request, 'login_and_register.html', {'stay': True, 'showRegister':True})
+            return  render(request, 'login_and_register.html', {'stay': True, 'showRegister':True, 'data': data})
 
         user = User.objects.create_user(username=username, email=email, password=password)
         user.save() # save the user
